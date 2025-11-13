0d940ee98d0760368a118a505fc527f8cadc5b61
MR-Valdez <khadwiz@gmail.com>
2025-11-13 15:33:05 -0600

    adjusted mage values from PTR notes

--- sim/mage/frost/hotfixes.go
+++ sim/mage/frost/hotfixes.go
@@ -6,10 +6,10 @@ import (
 )
 
 func (frost *FrostMage) registerHotfixes() {
-	// 2025-09-22 - Frostbolt/Frostfire bolt damage increased by 15%
+	// 2025-09-22 - Frostbolt/Frostfire/IceLance damage increase to lowered to 5%. (was 15%)
 	frost.AddStaticMod(core.SpellModConfig{
 		ClassMask:  mage.MageSpellFrostbolt | mage.MageSpellFrostfireBolt | mage.MageSpellIceLance,
 		Kind:       core.SpellMod_DamageDone_Pct,
-		FloatValue: 0.15,
+		FloatValue: 0.05,
 	})
 }
